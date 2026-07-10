8cab15233a6e52a6628bc5c90d2c98efb34bc659
Valentina Ferro <valentina-ferro@hotmail.it>
2026-07-10 13:39:54 +0200

    Chiamare getForecast in WeatherDetail e salvarlo nello stato

--- src/pages/WeatherDetail.jsx
+++ src/pages/WeatherDetail.jsx
@@ -1,12 +1,13 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-import { getCurrentWeather } from "../api/weatherApi";
+import { getCurrentWeather, getForecast } from "../api/weatherApi";
 
 const WeatherDetail = () => {
   const { city } = useParams();
 
   // 3 stati: weather (null iniziale), loading (bool), error (null iniziale)
   const [weather, setWeather] = useState(null);
+  const [forecast, setForecast] = useState([]);
   const [loading, setLoading] = useState(true);
   // dato che la fetch parte subito al montaggio del componente,
   // conviene partire già in stato "loading" per evitare
@@ -19,9 +20,15 @@ const WeatherDetail = () => {
       // reset prima di ogni chiamata
 
       try {
-        const data = await getCurrentWeather(city);
-        setWeather(data);
+        const [currentData, forecastData] = await Promise.all([
+          getCurrentWeather(city),
+          getForecast(city),
+        ]);
+
+        setWeather(currentData);
+        setForecast(forecastData.list);
         // salva data nello stato weather
+        // il campo "list" è l'array con le previsioni ogni 3 ore
       } catch (err) {
         setError(err.message);
       } finally {
